f729971e45a385a6c6d6ebced6e150219ddc03ce
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-03-21 09:47:52 -0300

    First Commit

---
+++ Public/php/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <title>Combo+ | Oferta especial que inclui Disney+ e Star+</title>
+
+</head>
+<body>
+
+    <a href="#" class="btn btn-lg button-entrar-class">ENTRAR</a>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-3">
+                <img class="container-img-starPlus" src="../../Private/image/disneyComboStar.png">
+                <div class="d-grid gap-2 containter-button-saveAndGet">
+                    <a href="#" class="btn btn-lg button-saveAndGet">SAVE AND GET THEM BOTH</a>
+                </div>
+                <p class="p-assinar-class">
+                    <a href="#">Assinar somente o Disney+</a>
+                </p>
+                <p class="p-assinar-class">
+                    <a href="">Assinar somente o Star+</a>
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+
+</body>
+</html>
