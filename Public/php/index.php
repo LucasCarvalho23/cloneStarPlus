d8d1e3cd8b757fb10a7bfe56e56209b205e98c24
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-03-23 02:14:06 -0300

    perguntasFrequentes1

--- Public/php/index.php
+++ Public/php/index.php
@@ -30,7 +30,7 @@
             </div>
 
             <div class="col-8">
-                <img class="containter-img-catalogo" src="../../Private/image/catalogo-main.png">
+                <img class="containter-img-catalogo" src="../../Private/image/catalogo-main.png" width="1100px">
             </div>
 
         </div>
@@ -100,14 +100,151 @@
         </div>
     </div>
 
-    <div class="container-catalogo-third">
-        <h3>Histórias que você só encontra no Disney+</h3>
-        <p>Descubra as melhores histórias da Disney, Pixar, Marvel, Star Wars e National Geographic.</p>
-        <div>
-            <img src="../../Private/image/catalogo-thr.png">
+    <div class="container">
+        <div class="row">
+        <div class="container-catalogo-third">
+            <h3>Histórias que você só encontra no Disney+</h3>
+            <p>Descubra as melhores histórias da Disney, Pixar, Marvel, Star Wars e National Geographic.</p>
+            <div>
+                <img src="../../Private/image/catalogo-thr.png">
+            </div>
+        </div>
         </div>
     </div>
 
+    <div class="container">
+        <div class="row">
+
+            <div class="container-pergFrequentes">
+                <h3>Perguntas Frequentes</h3>
+                <br>
+            
+                <div class="accordion" id="accordionpergFrequentes">
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            <strong>Já tem o Disney+ ou o Star+ e quer o Combo+?</strong>
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                        <div class="accordion-body">
+                            <p>Ao assinar o Combo+:</p>
+                            <ul>
+                                <li>O valor da sua assinatura existente será descontado do preço do Combo+.</li>
+                                <li>Se você assina o plano mensal, o valor que você paga mensalmente será descontado do preço do Combo+.</li>
+                                <li>Se você tem uma assinatura anual do Disney+ cobrada diretamente pelo Disney+, não é possível mudar para o Combo+ no momento. Entre em contato com o suporte do Star+ para conhecer suas opções.</li>
+                                <li>Você vai receber um e-mail com os valores detalhados assim que finalizar o pagamento.</li>
+                                <li>Lembre-se: você pode cancelar a assinatura quando quiser.</li>
+                            </ul>
+                            <p>No momento, apenas a assinatura mensal do Combo+ está disponível.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <strong>Já é assinante do Disney+?</strong>
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                        <div class="accordion-body">
+                            <p>Assine o Star+ usando o mesmo login e senha do Disney+, assim você terá acesso a ambos os serviços por um preço promocional. Para ser assinante, você deve ter 18 anos.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <strong>Em quantos dispositivos posso assistir ao mesmo tempo?</strong>
+                        </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                        <div class="accordion-body">
+                            <p>Com o Combo+, você tem entretenimento em dobro. Você pode assistir ao Disney+ em 4 dispositivos compatíveis simultaneamente e ao Star+ em mais 4.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            <strong>Posso baixar conteúdo?</strong>
+                        </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                        <div class="accordion-body">
+                            <p>Com o Combo+, você pode baixar conteúdo do Disney+ em 10 dispositivos e do Star+ em mais 10.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            <strong>Quais formas de pagamento posso utilizar?</strong>
+                        </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                        <div class="accordion-body">
+                            <p>Você pode utilizar: cartão de débito, cartão de crédito, PayPal, Mercado Pago, e também empresas parceiras como Google Play Store, App Store da Apple; Mercado Livre e provedores locais de TV, telefonia e internet.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                            <strong>O Star+ possui controle parental?</strong>
+                        </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                        <div class="accordion-body">
+                            <p>Sim. Com o controle parental do Star+, você poderá controlar o acesso a conteúdos de acordo com a classificação indicativa selecionada, oferecendo maior segurança aos usuários da sua família.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="accordion-item container-accordion-class">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                <strong>Onde posso assistir ao Disney+?</strong>
+                            </button>
+                        </h2>
+                        <div id="collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionpergFrequentes">
+                            <div class="accordion-body">
+                                <p>O aplicativo do Disney+ está disponível em dispositivos móveis, navegadores, videogames, decodificadores e smart TVs. Confira <a href="#">a lista completa de dispositivos compatíveis na Central de Ajuda.</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+
+                </div>
+            
+            </div>
+
+        </div>
+    </div>
+
+    <footer style="color: white; margin-top: 100px">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    FOOTER
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 </body>
 </html>
